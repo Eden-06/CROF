912625613e7fda7b43c18e41132a3f1cb8967c15
zhenxizhao <zhenxizhao@localhost>
2018-03-28 23:37:08 +0200

    Current version: 0.3.0
    Current features:
    
     1 new CROM Project Wizard
     2 crom2java popupMenus
     3 Natural Type generator
     4 Data Type generator
     5 Compartment Type generator
     6 Role Type generator
       6.1 Role Group can be recursive generated
     7 Fulfillment Relation has been realized
    
    
    following work：
    
     1 Relation Ship realize
     2 Inheritance Relation realize
     3 load instance
     4 store instance

---
+++ org.rosi.crom.framework/src/org/rosi/crom/framework/generator/main/templates/Util.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package org.rosi.crom.framework.generator.main.templates;
+
+import java.util.Set;
+import java.io.File;
+import java.util.HashSet;
+import org.eclipse.emf.common.util.URI;
+import crom_l1_composed.RoleType;
+import crom_l1_composed.impl.RoleTypeImpl;
+
+/**
+ * This is a simple utility class to store and retrieve the modelURI of the
+ * currently processed Ecore model instance (xmi). Most importantly it defines a
+ * getModelUri() method that emits a corresponding string representation.
+ * 
+ * @author eden06
+ *
+ */
+public class Util {
+
+	public static URI modeluri = null;
+	public static File filePath = null;
+	public static String folderPath = "";
+	public static String packagePath = "";
+	public static Set<RoleType> thisset = new HashSet<RoleType>();
+
+	public Util() {
+	}
+
+	/**
+	 * Set the modelUri as static variable accessed via the Util objects.
+	 * 
+	 * @param m
+	 *            the modelURI to be set
+	 */
+	public static void setModelUri(URI m) {
+		modeluri = m;
+		if (m != null)
+			System.err.println("modelpath: " + m.toString());
+	}
+
+	/**
+	 * Returns the last modelURI that has been set globally, via either of the Util
+	 * objects, or "model", if static variable is undefined (or set to null).
+	 * 
+	 * @return a string representation of the modelURI or simply the String "model"
+	 *         if no modeluri was set.
+	 */
+	public String getModelUri() {
+		if (modeluri == null)
+			return "model";
+		return modeluri.toString();
+	}
+
+	public static void setFilePath(File m) {
+		filePath = m;
+		if (m != null)
+			System.err.println("modelpath: " + m.toString());
+	}
+
+	/**
+	 * Returns the last modelURI that has been set globally, via either of the Util
+	 * objects, or "model", if static variable is undefined (or set to null).
+	 * 
+	 * @return a string representation of the modelURI or simply the String "model"
+	 *         if no modeluri was set.
+	 */
+	public String getFilePath() {
+		if (filePath == null)
+			return "model";
+		return filePath.toString();
+	}
+
+	public static void setPackagePath(String str) {
+		packagePath = str;
+		// if (str != null)
+		// System.err.println("modelpath: " + str.toString());
+	}
+
+	public static void setFolderPath(String str) {
+		folderPath = str;
+		// if (str != null)
+		// System.err.println("modelpath: " + str.toString());
+	}
+
+	public String getPackagePath() {
+		if (packagePath == "")
+			return "package";
+		return packagePath;
+	}
+
+	public String getFolderPath() {
+		if (folderPath == "")
+			return "folder";
+		return folderPath;
+	}
+
+	public boolean setRole(RoleTypeImpl role) {
+
+		return thisset.add((RoleType) role);
+
+	}
+
+	public Set<RoleType> getRole() {
+
+		return thisset;
+
+	}
+
+}
